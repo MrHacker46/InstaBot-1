f5a1ea865b0f9b8d8e322a07049abe8794e7f511
tx f1erce <txf1ercetx@gmail.com>
2018-09-04 15:36:42 -0500

    Updated README.md

--- instagram_bot.py
+++ instagram_bot.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 """
+# instagram_bot
+Instagram bot that likes a users picture.
+
 This is an instagram bot.
-This program scrolls through a certain hashtag and likes n pictures.
+This program scrolls through a certain hashtag, e.g(#linux, #programming, etc) and likes `n` pictures.
 It's main purpose is to gain followers without being banned or using fake accounts.
 As of 2018-09-04, the current likes per hour rate limit from instagram is: 250.
-You can fork, copy this program as you like as long as you credit the author (https://github.com/n30-14/instagram_bot)
+You can fork, copy this program as you like as long as you credit the author (https://github.com/n30-14/instagram_bot) 
 Constructive criticism welcomed... Enjoy! :)
 """
 import time, sys, os, random
@@ -65,7 +68,7 @@ class Instagram_Bot:
 
         # Push enter (RETURN KEY)
         upass.send_keys(Keys.RETURN)
-        time.sleep(3) # Modify this if your account requires two-factor authentication or locked out
+        time.sleep(60) # Modify this if your account requires two-factor authentication or locked out
 
     # This method clicks on the user photo and hearts it.
     # It then clicks the right arrow to move to the next photo <3
