3308add48788bf4ce060dac2ac8e173b573baa50
tx f1erce <txf1ercetx@gmail.com>
2018-09-04 14:59:24 -0500

    Instagram bot...

--- instagram_bot.py
+++ instagram_bot.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 This is an instagram bot.
 This program scrolls through a certain hashtag and likes n pictures.
@@ -6,7 +7,6 @@ As of 2018-09-04, the current likes per hour rate limit from instagram is: 250.
 You can fork, copy this program as you like as long as you credit the author (https://github.com/n30-14/instagram_bot)
 Constructive criticism welcomed... Enjoy! :)
 """
-#!/usr/bin/env python
 import time, sys, os, random
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
