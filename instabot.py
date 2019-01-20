fe4837bd8ac44e7a073b650541d7bacc060c78c6
n30-14 <txf1ercetx@gmail.com>
2019-01-20 14:28:33 -0600

    Major updates to src...

--- instabot.py
+++ instabot.py
@@ -12,18 +12,17 @@ def main():
 
     try:
 	# Login user
-	login(driver)
+        login(driver)
 
 	# Open file
-	with open(sys.argv[3], "r") as f:
-	    while True:
-		hashtag = f.readline()
-
-		if hashtag == '':
-		    f.seek(0)
-		    time.sleep(1)
-		else:
-		    like_photo(driver, hashtag)
+        with open(sys.argv[3], "r") as f:
+            while True:
+                hashtag = f.readline()
+                if hashtag == '':
+                    f.seek(0)
+                    time.sleep(1)
+                else:
+                    like_photo(driver, hashtag)
 
     except KeyboardInterrupt:
     	print("[+] CTRL+C Pressed... Exiting the program...")
@@ -32,42 +31,42 @@ def main():
 # This method displays usage message to the user...
 def usage(argc):
     if argc != 4:
-    	print("Usage: {} <username> <password> <hashtag>\n".format(sys.argv[0]))
-    	print("Options:")
-	print("  username:  your instagram username you want to use to login")
-	print("  password:  password of the instagram account")
-	print("  hashtag :  a hashtag or a wordlist file containing hashtags to use\n")
-	print("Example:")
-	print("  {} ___ch1pmunx___ passtheword123 hashtag.txt".format(sys.argv[0]))
-	sys.exit(1)
+        print("Usage: {} <username> <password> <hashtag>\n".format(sys.argv[0]))
+        print("Options:")
+        print("  username:  your instagram username you want to use to login")
+        print("  password:  password of the instagram account")
+        print("  hashtag :  a hashtag or a wordlist file containing hashtags to use\n")
+        print("Example:")
+        print("  {} ___ch1pmunx___ passtheword123 hashtag.txt".format(sys.argv[0]))
+        sys.exit(1)
 
 # This method is used to login the user...
 def login(driver):
     try:
 	# driver = webdriver.Firefox()
 	# driver.get("https://www.instagram.com/")
-	driver.get("https://www.instagram.com/accounts/login/?source=auth_switcher")
+        driver.get("https://www.instagram.com/accounts/login/")
 
 	# Wait for page to load...
-	time.sleep(3)
+        time.sleep(3)
 
 	# Enter username
-	username = driver.find_element_by_xpath("//input[@name='username']")
-	username.clear()
-	username.send_keys(sys.argv[1])
+        username = driver.find_element_by_xpath("//input[@name='username']")
+        username.clear()
+        username.send_keys(sys.argv[1])
 
-	# Enter password
-	password = driver.find_element_by_xpath("//input[@name='password']")
-	password.clear()
-	password.send_keys(sys.argv[2])
-	password.send_keys(Keys.RETURN)
+        # Enter password
+        password = driver.find_element_by_xpath("//input[@name='password']")
+        password.clear()
+        password.send_keys(sys.argv[2])
+        password.send_keys(Keys.RETURN)
 
-	print("[+] Logged in...")
-	time.sleep(3)
+        print("[+] Logged in...")
+        time.sleep(3)
 
     except Exception:
-    	print("[!] Invalid credentials...")
-	sys.exit(1)
+        print("[!] Invalid credentials...")
+        sys.exit(1)
 
 # This method clicks on the photo, <3's it, and goes the next photo
 def like_photo(driver, hashtag):
@@ -83,42 +82,42 @@ def like_photo(driver, hashtag):
 
     i=0
     while True:
-    	print("[+] Current photo: {}".format(i))
+        print("[+] Current photo: {}".format(i))
 
-	if i > randint(144, 188):
-	    print("[+] {} <3 reached!...".format(i))
-	    print("[+] Taking a 30 min nap...")
-	    print("[+] Time stopped: {}".format(datetime.datetime.now()))
-	    time.sleep(1800)
-	    break
+        if i > randint(144, 188):
+            print("[+] {} <3 reached!...".format(i))
+            print("[+] Taking a 30 min nap...")
+            print("[+] Time stopped: {}".format(datetime.datetime.now()))
+            time.sleep(1800)
+            break
 
-	# <3 the first photo and go to next...
-	try:
-	    print("  - Trying to <3 the photo...")
-	    like_photo = driver.find_element_by_xpath("//span[@class='glyphsSpriteHeart__outline__24__grey_9 u-__7']")
-	    like_photo.click()
-	    time.sleep(1)
+        # <3 the first photo and go to next...
+        try:
+            print("  - Trying to <3 the photo...")
+            like_photo = driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/article/div[2]/section[1]/span[1]/button")
+            like_photo.click()
+            time.sleep(1)
 
-	    # Go to next photo
-    	    print("  - Next photo...")
-	    next_photo = driver.find_element_by_xpath("//a[@class='HBoOv coreSpriteRightPaginationArrow']")
-	    next_photo.click()
-    	    time.sleep(4)
+            # Go to next photo
+            print("  - Next photo...")
+            next_photo = driver.find_element_by_xpath("//a[@class='HBoOv coreSpriteRightPaginationArrow']")
+            next_photo.click()
+            time.sleep(4)
 
-	    # Increment `i`
-	    i+=1
-	except Exception:
-	    print("  - Photo already has <3... Next photo...")
-	    try:
-		next_photo = driver.find_element_by_xpath("//a[@class='HBoOv coreSpriteRightPaginationArrow']")
-		next_photo.click()
-		time.sleep(2)
-    		# HERE IS THE BUG
-    	    except:
-	    	print("[!] Locating right arrow!!!!")
-	    	# Go back to the start of the page
+            # Increment `i`
+            i+=1
+        except Exception:
+            print("  - Photo already has <3... Next photo...")
+            try:
+                next_photo = driver.find_element_by_xpath("//a[@class='HBoOv coreSpriteRightPaginationArrow']")
+                next_photo.click()
+                time.sleep(2)
+                # HERE IS THE BUG
+            except:
+                print("[!] Locating right arrow!!!!")
+                # Go back to the start of the page
 		# driver.get("https://www.instagram.com/explore/tags/" + hashtag)
-		time.sleep(5)
+                time.sleep(5)
 		# Click the first photo
 		# first_photo = driver.find_element_by_xpath("/html/body/span/section/main/article/div[1]/div/div/div[1]/div[1]/a")
 		# driver.execute_script("arguments[0].click();", first_photo)
